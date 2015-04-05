314039aba1ec56311effa2a0b0ab458c0e023975
Paulo Sergio <paulofranca.contato@gmail.com>
2015-04-05 00:29:48 -0300

    termino do index completo
    fim do treinamento com o index completo!!

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,20 @@
 jQuery(document).ready(function($){
     
+	//filtro jquery para portfolio
 	$('.portfolio_items').mixItUp();
+	
+	//carrousel para mensagens...
+	$('.testimonial').owlCarousel({
+		items: 1,
+		singleItem : true,
+		pagination : false,
+		navigation : true,
+		navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"]
+	});
+	
+	//parallax effect
+	//$('.click_sharea_bg').parallax("50%",0.1);
+	//$('.uniq_bg').parallax("50%",0.1);
+	//$('.testimonial_bg').parallax("50%",0.1);
 	
 });
